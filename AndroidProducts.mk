a32c528707c21755f48e75dbaab3a828ed1bb7cc
hdrd13 <96956239+hdrd13@users.noreply.github.com>
2023-08-25 00:38:27 +0300

    Bringup YAAP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_certus.mk
+    $(LOCAL_DIR)/yaap_certus.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_certus-user \
-    nad_certus-userdebug \
-    nad_certus-eng
+    yaap_certus-user \
+    yaap_certus-userdebug \
+    yaap_certus-eng
